b2b6451e49a95085f53f585b9575b05e5a845cd7
Santiago Gutierrez <santiago.gutierrezyg@gmail.com>
2025-08-28 12:15:05 -0500

    feat: Se crea el endpoint para traer todos los productos

--- mi_backend/app/models/Product.py
+++ mi_backend/app/models/Product.py
@@ -15,3 +15,16 @@ class Product(db.Model):
     created_at = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
     updated_at = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
     deleted_at = db.Column(db.DateTime, nullable=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "size": self.size,
+            "price": self.price,
+            "description": self.description,
+            "is_active": self.is_active,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+            "deleted_at": self.deleted_at
+        }
--- mi_backend/app/routes/product.py
+++ mi_backend/app/routes/product.py
@@ -4,10 +4,13 @@ from ..models import Product
 
 product_bp = Blueprint("product", __name__)
 
-@product_bp.route("/test", methods=["GET"])
+@product_bp.route("/", methods=["GET"])
 def test_db():
     try:
-        products = Product.query.all()
-        return jsonify({"ok":True, "count": len(products)})
+        products = Product.query.filter(
+            Product.deleted_at.is_(None)
+        ).all()
+        products_list = [p.to_dict() for p in products]
+        return jsonify({"ok":True, "products": products_list})
     except Exception as e:
         return jsonify({"ok": False, "error": str(e)})
