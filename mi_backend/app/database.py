486ac1e138f3f83e89feae563e0573fbdfb9b846
Santiago <santiago.gutierrezyg@gmail.com>
2025-10-29 21:21:57 -0500

    configurando el pool para conexiones muertas

--- mi_backend/app/database.py
+++ mi_backend/app/database.py
@@ -10,4 +10,12 @@ def init_db(app):
 
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    # Evita usar conexiones muertas
+    app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
+        "pool_pre_ping": True,        # chequea que la conexión siga viva antes de usarla
+        "pool_recycle": 280,          # recicla conexiones cada 5 min (antes del timeout del servidor)
+        "pool_timeout": 30,           # espera máx. 30 seg al obtener conexión del pool
+    }
+
     db.init_app(app)
